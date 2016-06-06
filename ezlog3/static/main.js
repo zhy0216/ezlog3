c7f78d6e65d5345bf4ffdb90a1441365c9dba7da
Yang <zhy0216@gmail.com>
2016-06-06 08:32:18 -0400

    user can post text

--- ezlog3/static/main.js
+++ ezlog3/static/main.js
@@ -2,6 +2,19 @@
 Zepto(function($){
     console.log("ready!");
 
+    $("#tweet-textarea").on("keydown", function(e){
+        if(e.ctrlKey && e.which === 13){
+            var $textarea = $("#tweet-textarea");
+            var content = $textarea.val()
+            if(content !== ""){
+                $.post("/api/tweet/",{
+                    "content": content
+                }, function(data){
+                    console.log(data)
+                })
+            }
+        }
+    })
 
 })
 
