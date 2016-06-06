7449cfb2514d7b26029a9c8bfdaa9dbd6bf62745
Yang <zhy0216@gmail.com>
2016-06-06 18:13:59 -0400

    make login view

--- ezlog3/controller/frontend.py
+++ ezlog3/controller/frontend.py
@@ -8,12 +8,9 @@ import flask.ext.login as flask_login
 from ezlog3 import app, login_manager
 from ezlog3.model import User, Tweet
 
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    return username == 'admin' and password == 'secret'
 
+
+@login_manager.unauthorized_handler
 def authenticate():
     """Sends a 401 response that enables basic auth"""
     return Response(
@@ -21,10 +18,15 @@ def authenticate():
     'You have to login with proper credentials', 401,
     {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
-@login_manager.unauthorized_handler
-def unauthorized():
+@login_manager.request_loader
+def load_user_from_request(request):
     auth = request.authorization
-    return authenticate()
+    if auth:
+        user = User.validate_user(auth.username, auth.password)
+        if user:
+            flask_login.login_user(user)
+            return user
+
 
 @app.route("/")
 def main():
@@ -42,26 +44,13 @@ def config():
     # config page
     return "1"
 
-if app.config["DEBUG_MODE"]:
-    @app.route("/debug/login")
-    def debug_login():
-        flask_login.login_user(User.objects().first())
-        return flask.redirect(flask.url_for('main'))
-
-
-
-
-@login_manager.request_loader
-def load_user_from_request(request):
-    api_key = request.headers.get('Authorization')
-    return None
-    user = User.objects().first()
-    flask_login.login_user(user)
-    return user
+@app.route("/login")
+@flask_login.login_required
+def login():
+    return flask.redirect("/")
 
 
 @app.route("/logout")
-@flask_login.login_required
 def logout():
     flask_login.logout_user()
-    return flask.redirect(flask.url_for('main'))
+    return authenticate()
